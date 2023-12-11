eff00afa06c2d7c838c82951c3c3c4afc245a57e
tamang-manoj <tamang.manoj@webexpertsnepal.com>
2023-12-11 15:27:50 +0545

    vercel checking

--- src/components/ProtectedAdmin.tsx
+++ src/components/ProtectedAdmin.tsx
@@ -1,20 +1,24 @@
 import { Outlet } from "react-router";
 import Login from "../features/character/Login";
-import { useAppSelector } from "../app/hooks";
+// import { useAppSelector } from "../app/hooks";
 
 const ProtectedAdmin = () => {
-  // const value = localStorage.getItem("persist_login");
-  // let persistedLog;
-  // if (value) {
-  //   persistedLog = JSON.parse(value);
-  // }
-  // //   console.log(value);
+  const value = localStorage.getItem("persist_login");
+  let persistedLog;
+  if (value) {
+    persistedLog = JSON.parse(value);
+  }
+  //   console.log(value);
 
-  const status = useAppSelector((state) => state.characters.status);
+  // const status = useAppSelector((state) => state.characters.status);
 
   return (
     <div>
-      {status.isLoggedIn && status.role === "admin" ? <Outlet /> : <Login />}
+      {persistedLog.isLoggedIn && persistedLog.role === "admin" ? (
+        <Outlet />
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
